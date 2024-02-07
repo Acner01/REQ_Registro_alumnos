f37b5f1d40c8994a4ce63f35dd69143622ea99f1
Acner01 <Acner.s315492@cesurformacion.com>
2024-02-07 20:41:15 +0100

    Primer Commit

---
+++ src/app/shared/components/custom-form/custom-form.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomFormComponent } from './custom-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    CustomFormComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CustomFormModule { }
